889e7218784176659fec27bad216ae70e7e31758
J0n1i <joni.puitti@outlook.com>
2025-03-16 20:12:03 +0200

    migrate from packer to lazy

--- lua/config/remap.lua
+++
@@ -1,9 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-
-vim.api.nvim_set_keymap('n', '<leader>cp', ':Copilot<CR>', { noremap = true, silent = true })
-
-
-vim.keymap.set('n', '<C-s>', ':w<CR>', { desc = 'Save file' })
-
-vim.keymap.set('n', '<F5>', ':so %<CR>', { desc = 'Source current file' })
---
+++ lua/keymaps.lua
@@ -0,0 +1,6 @@
+-- Keymaps
+vim.keymap.set("n", "<C-s>", ":w<CR>", {})
+vim.keymap.set("n", "<F5>", ":so %<CR>", {})
+vim.keymap.set("n", "<leader>e", ":Ex<CR>", {})
+vim.keymap.set("n", "<leader>q", ":q<CR>", {})
+vim.keymap.set("n", "<C-a>", "ggVG", {})
